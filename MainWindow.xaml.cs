8c59710ec8863a67021b12748a68a4346949b685
Ramin <Ramin@DESKTOP-CQSA626>
2022-03-08 13:56:56 +0330

    Fixed Saving Path

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using AdonisUI.Controls;
 using MessageBox = AdonisUI.Controls.MessageBox;
 using MessageBoxButton = AdonisUI.Controls.MessageBoxButton;
 using MessageBoxImage = AdonisUI.Controls.MessageBoxImage;
+using System.Security;
 
 namespace TextTools
 {
@@ -33,54 +34,105 @@ namespace TextTools
         }
         string[] TextFile;
         string[] TextFiles = {};
-        string path;
         string filename;
 
         private void btn_opentextfile_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-            if (openFileDialog.ShowDialog() == true)
-            TextFile = File.ReadAllLines(openFileDialog.FileName);
-            txt_numberline.Text = "Number Of Lines: " + TextFile.Count().ToString();
-            txt_selectedfile.Text = "Selected File: " + openFileDialog.SafeFileName;
-            path = openFileDialog.FileName;
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == true)
+                    TextFile = File.ReadAllLines(openFileDialog.FileName);
+                txt_numberline.Text = "Number Of Lines: " + TextFile.Count().ToString();
+                txt_selectedfile.Text = "Selected File: " + openFileDialog.SafeFileName;
+            }
+            catch (SecurityException ex)
+            {
+                MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                    "Error message: " + ex.Message + "\n\n" +
+                    "Details (send to Support):\n\n" + ex.StackTrace
+                );
+            }
         }
 
         private void btn_remove_Click(object sender, RoutedEventArgs e)
         {
-            string[] TextFile_nodup = TextFile.Distinct().ToArray();
-            File.WriteAllLines(path, TextFile_nodup);
-            txt_numberline.Text = "Number Of Lines: " + TextFile_nodup.Count().ToString();
-            MessageBox.Show("Done!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                saveFileDialog.FileName = "Removed";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    string[] TextFile_nodup = TextFile.Distinct().ToArray();
+                    File.WriteAllLines(saveFileDialog.FileName, TextFile_nodup);
+                    txt_numberline.Text = "Number Of Lines: " + TextFile_nodup.Count().ToString();
+                }
+                MessageBox.Show("Done!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (SecurityException ex)
+            {
+                MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                    "Error message: " + ex.Message + "\n\n" +
+                    "Details (send to Support):\n\n" + ex.StackTrace
+                );
+            }
         }
 
         private void btn_selectfolder_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-            openFileDialog.Multiselect = true;
-            if (openFileDialog.ShowDialog() == true)
+            try
             {
-                foreach (String file in openFileDialog.FileNames)
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.Multiselect = true;
+                if (openFileDialog.ShowDialog() == true)
                 {
-                    string[] temp = File.ReadAllLines(file);
-                    TextFiles = TextFiles.Union(temp).ToArray();
+                    foreach (String file in openFileDialog.FileNames)
+                    {
+                        string[] temp = File.ReadAllLines(file);
+                        TextFiles = TextFiles.Union(temp).ToArray();
+                    }
+                    foreach (String file in openFileDialog.SafeFileNames)
+                    {
+                        filename += file + ", ";
+                    }
                 }
-                foreach (String file in openFileDialog.SafeFileNames)
-                {
-                    filename += file + ", ";
-                }
-
+                txt_selectedfiles.Text = "Selected Files: " + filename;
+                txt_numberfiles.Text = "Number Of Files: " + openFileDialog.FileNames.Count().ToString();
             }
-            txt_selectedfiles.Text = "Selected Files: " + filename;
-            txt_numberfiles.Text = "Number Of Files: " + openFileDialog.FileNames.Count().ToString();
+            catch (SecurityException ex)
+            {
+                MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                    "Error message: " + ex.Message + "\n\n" +
+                    "Details (send to Support):\n\n" + ex.StackTrace
+                );
+            }
         }
 
         private void btn_combine_Click(object sender, RoutedEventArgs e)
         {
-            File.WriteAllLines(@"C:\Users\Ramin\Desktop\combined.txt", TextFiles);
-            MessageBox.Show("Done!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+                saveFileDialog.InitialDirectory = AppDomain.CurrentDomain.BaseDirectory;
+                saveFileDialog.FileName = "Combined";
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    File.WriteAllLines(saveFileDialog.FileName, TextFiles);
+                }
+                MessageBox.Show("Done!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (SecurityException ex)
+            {
+                MessageBox.Show("Security error. Please contact your administrator for details.\n\n" +
+                    "Error message: " + ex.Message + "\n\n" +
+                    "Details (send to Support):\n\n" + ex.StackTrace
+                );
+            }
         }
     }
 }
